65ec10aea8e80a9306a560b38434cf2857d29af1
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2018-03-06 16:46:15 +0100

    Add and fix new card and column funcionalities

--- card.js
+++ card.js
@@ -1,9 +1,10 @@
 // KLASA KANBAN CARD
-function Card(id, name) {
+function Card(id, name, columnId) {
 	var self = this;
 	this.id = id;
 	this.name = name || 'No name given';
 	this.element = createCard();
+	this.columnId = columnId;
 
 	function createCard() {
 		var card = $('<li class="card"></li>');
@@ -47,11 +48,12 @@ Card.prototype = {
 			url: baseUrl + '/card/' + self.id,
 			method: 'PUT',
 			data: {
+				id: self.id,
 		        name: newCardName,
-    			bootcamp_kanban_column_id: self.id
+		        bootcamp_kanban_column_id: self.columnId
 		    },
 		    success: function(response) {
-		        self.cardName = newCardName;
+		        $(self.element).find(".card-name").text(newCardName);
 		    }
 
 		});
--- column.js
+++ column.js
@@ -7,14 +7,19 @@ function Column(id, name) {
 	function createColumn() {
 		// TWORZENIE NOWYCH WĘZŁÓW
 		var column = $('<div class="column"></div>');
-		var columnTitle = $('<h2 class="column-title">' + self.name + '</h2>');
+		var columnName = $('<h2 class="column-name">' + self.name + '</h2>');
 		var columnCardList = $('<ul class="card-list"></ul>');
 		var columnDelete = $('<button class="btn-delete">x</button>');
 		var columnAddCard = $('<button class="column-add-card">Dodaj kartę</button>');
+		var columnRename = $('<button class="btn-rename">rename column</button>');
 
 		// PODPINANIE ODPOWIEDNICH ZDARZEŃ POD WĘZŁY
 		columnDelete.click(function() {
 			self.deleteColumn();
+		});
+
+		columnRename.click(function(){
+			self.renameColumn();
 		});
 
 		columnAddCard.click(function(event) {
@@ -28,17 +33,18 @@ function Column(id, name) {
     				bootcamp_kanban_column_id: self.id
 		        },
 		        success: function(response) {
-		            var card = new Card(response.id, cardName);
+		            var card = new Card(response.id, cardName, self.id);
         			self.createCard(card);
 		        }
 		    });
 		});
 
 			// KONSTRUOWANIE ELEMENTU KOLUMNY
-		column.append(columnTitle)
+		column.append(columnName)
 			.append(columnDelete)
 			.append(columnAddCard)
-			.append(columnCardList);
+			.append(columnCardList)
+			.append(columnRename);
 			return column;
 		}
 }
@@ -57,7 +63,26 @@ Column.prototype = {
 	        self.element.remove();
 	      }
 	    });
-	 }
+	},
+
+	renameColumn: function() {
+		var self = this;
+		var newColumnName = prompt("Enter new name of the column");
+		event.preventDefault();
+		$.ajax({
+			url: baseUrl + '/column/' + self.id,
+			method: 'PUT',
+			data: {
+				id: self.id,
+		        name: newColumnName
+		    },
+		    success: function(response) {
+		        $(self.element).find(".column-name").text(newColumnName);
+		    }
+
+		});
+
+	}
 
 };
 
